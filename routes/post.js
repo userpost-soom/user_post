d78caa52b6a630f8b033a9ec72f0a1932e7c8b8e
숨 <ksoom0207@gmail.com>
2022-01-22 03:09:11 +0900

    post 쿼리 일부수정

--- routes/post.js
+++ routes/post.js
@@ -32,14 +32,14 @@ mysqlCon.open(postquery);
 //저장 되어 있는 게시글 불러오기 
 router.get('/', (req, res, next) => {
     try { //delete_time이 없는 즉, 삭제되지 않은 게시글 리스트를 보여줌
-        postquery.query('select idx, title, content, user_id, update_time from board_table where delete_time is null order by idx asc',
+        postquery.query('select idx, title, content, user_id, upload_time from board_table where delete_time is null order by idx asc',
             (err, result, field) => {
                 if (err) res.status(401);
-                res.status(200).send(result);
+                return res.status(200).send(result);
             });
 
     } catch (error) {
-        res.status(401).send({ error: error.message });
+        return res.status(401);
     }
 });
 
@@ -54,22 +54,23 @@ router.post('/write', (req, res) => {
 
     let board_password = newWrite.board_pass;
     //게시글 입력 쿼리 보내기
-    if (req.body.title && req.body.content && req.body.user_id) { //세가지의 값이 존재할 경우
-        if (newWrite.board_pass) { //게시글 비밀번호가 있을 경우
-            let hashPassword = crypto.createHash("sha256").update(board_password).digest("hex");
-            postquery.query(`INSERT INTO board_table SET title = '${newWrite.title}' , content ='${newWrite.content}', user_id ='${newWrite.user_id}', board_pass = '${newWrite.board_pass}'`,
-                (err, result) => {
-                    if (err) { return res.status(400); }
-                    else return res.status(201);
-                });
-        }
+    if (!req.body.title || !req.body.content || !req.body.user_id) return res.status(400).send("there_is_blank_info");
+
+    if (newWrite.board_pass) { //게시글 비밀번호가 있을 경우
+        let hashPassword = crypto.createHash("sha256").update(String(board_password)).digest("hex");
+        postquery.query(`INSERT INTO board_table SET title = '${newWrite.title}' , content ='${newWrite.content}', user_id ='${newWrite.user_id}', board_pass = '${hashPassword}'`,
+            (err, result) => {
+                if (err) { return res.status(400); }
+                return res.status(201).send("");
+            });
+    }
+    else {
         //게시글 비밀번호가 없을 경우
         postquery.query('INSERT INTO board_table SET ?', newWrite, (err, row) => {
             if (err) { return res.status(400); }
-            else return res.status(201);
-
+            return res.status(201).send("");
         });
-    } else return res.status(400).send("there_is_blank_info");
+    }
 
 });
 
@@ -78,48 +79,42 @@ router.get('/:idx', (req, res) => {
     //idx의 번호를 가진 게시글을 보여줌 
     let password = req.body.password;
 
-    postquery.query('select idx, title, content, user_id, board_pass, update_time from board_table where idx = ?', parseInt(req.params.idx), (err, result, field) => {
-        if (err) res.status(400).send(err.sqlMessage);
+    postquery.query('select idx, title, content, user_id, board_pass, upload_time from board_table where idx = ?', parseInt(req.params.idx), (err, result, field) => {
+        if (err) res.status(400).send(err);
         // 없는 인덱스 페이지를 불러올경우
-        if (!result[0]) return res.status(404);
+        if (!result[0]) return res.status(404).send("");
         //패스워드가 있을 경우
-        if (result.board_pass) {
+        if (result[0].board_pass) {
             let dbpassword = result[0].board_pass;
-            if (password === dbpassword) return res.status(200).send(result[0].idx, result[0].title, result[0].content, result[0].user_id, result[0].update_time);
+            if (password === dbpassword) return res.status(200).send(result[0]);
             else return res.status(401).send("wrong_password");
         }
-        return res.status(200).send(result[0].idx, result[0].title, result[0].content, result[0].user_id, result[0].update_time);
+        return res.status(200).send(result[0]);
     });
 });
 
 //수정하기 버튼을 눌렀을 경우
 router.put('/:idx', (req, res) => {
-    let modifyWrite = {
-        "title": req.body.title,
-        "content": req.body.content,
-        // "user_id": req.body.user_id, //idx
-        "board_pass": req.body.board_pass
-    };
+    let idx = parseInt(req.params.idx);
 
-    if (!req.body.title || !req.body.content || !req.body.user_id) return res.status(400).send("there_is_blank");
+    if (!req.body.title || !req.body.content) return res.status(400).send("there_is_blank");
 
-
+    //비밀번호 추가 or 변경 
     if (req.body.board_pass) {
-        let board_password = req.body.board_pass
+        let board_password = req.body.board_pass;
         let hashPassword = crypto.createHash("sha256").update(board_password).digest("hex");
         //수정한 내용과 시간을 같이 보내줌
-        postquery.query('UPDATE board_table SET title = ?, content = ?, board_pass =?  modify_time = current_timestamp() where idx = ?'
-            , [modifyWrite.title, modifyWrite.content, modifyWrite.board_pass, parseInt(req.params.idx)], (err, result) => {
-                if (err) { console.log(err); return req.status(400); }
-                res.status(201);
+        postquery.query(`UPDATE board_table SET title = '${req.body.title}', content = '${req.body.content}', board_pass = '${hashPassword}' , modify_time = current_timestamp() where idx = ?`
+            , idx, (err, result) => {
+                if (err) { console.log(err); return res.status(400); }
+                return res.status(200).send("");
             })
-
     }
     //패스워드가 없을 경우
     postquery.query('UPDATE board_table SET title = ?, content = ?, modify_time = current_timestamp() where idx = ?'
-        , [modifyWrite.title, modifyWrite.content, parseInt(req.params.idx)], (err, result) => {
-            if (err) { console.log(err); return req.status(400); }
-            res.status(201).send("success_modify");
+        , [req.body.title, req.body.content, idx], (err, result) => {
+            if (err) { console.log(err); return res.status(400); }
+            return res.status(200).send("");
         })
 });
 
@@ -130,12 +125,12 @@ router.delete('/:idx', (req, res) => {
     //인덱스 페이지가 없는경우
     postquery.query('select idx from board_table where idx = ?', idx,
         (err, result) => {
-            if (!result[0]) return res.status(404);
+            if (!result[0]) return res.status(404).send("");
             if (result[0]) {
                 postquery.query('update board_table SET delete_time = current_timestamp() where idx = ?', idx,
                     (err, result) => {
                         if (err) { return res.status(401); }
-                        return res.status(204);
+                        return res.status(204).send("");
                     });
             }
         });
