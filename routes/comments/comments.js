3a39766b43876a24e5cb2c926a955e35046aaf21
숨 <ksoom0207@gmail.com>
2022-01-28 00:56:54 +0900

    대댓글일부수정

--- routes/comments/comments.js
+++ routes/comments/comments.js
@@ -6,19 +6,23 @@ const req = require('express/lib/request');
 
 const commentquery = mysqlCon.init();
 
+let cmt = function (req, res) { }
+
 router.post('/', (req, res) => {
     let insert_query = `insert into comment_table SET ?`;
-    let idx = arseInt(req.params.idx);
-    req.body.post_idx = idx;
+    // let idx = ㅖarseInt(req.params.idx);
+    // req.body.post_idx = idx;
     commentquery.query(insert_query, req.body, (err, result, fleid) => {
         if (err) { console.log(err); return res.sendStatus(400); }
         return res.sendStatus(201);
     })
 });
 
-router.post('/reply', (req, res) => {
-    let insert_query = `insert into comment_table SET comment_parent_idx = '${req.body.comment_parent_idx}' ?`;
-    commentquery.query(insert_query, req.body, (err, result, fleid) => {
+//대댓글을 위한 쿼리
+router.post('/:comment_idx', (req, res) => {
+    let comment_idx = ParseInt(req.params.idx);
+    let insert_query = `insert into comment_table SET post_idx = ?, content = ?, user_id = ?, comment_parent_idx = ?`;
+    commentquery.query(insert_query, [req.body, comment_idx], (err, result, fleid) => {
         if (err) { console.log(err); return res.sendStatus(400); }
         return res.sendStatus(201);
     })
